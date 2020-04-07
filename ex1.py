35650b5f62c400e7d8a72b7ec8dc96ad5e8d75b8
shmulik.or <gh778apk>
2020-04-07 13:58:34 +0300

    msg

--- ex1.py
+++ ex1.py
@@ -1,21 +1,24 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def read_data():
     with open("data_EX1.txt") as file:
         data = np.loadtxt(file)
-        return data[:, 0], data[:, 1]
+        return data
+
+def pearson(data):
+    return np.corrcoef(data[:, 0], data[:, 1])[1][0]
 
 
-def permutation_test(x, y, n=1000, alpha=0.01):
-    cor_coef = np.corrcoef(x, y)[1][0]
-    print("Pearson correlation coefficient test is {}".format(cor_coef))
+def permutation_test(data, stat, n=10, alpha=0.01):
+    s = stat(data)
+    print("basic test is {}".format(s))
 
     counter = 0
     for i in range(0, n):
-        y_per = np.random.permutation(y)
-        counter = counter + 1 if np.corrcoef(x, y_per)[1][0] >= cor_coef else counter
-        # print("cor coef {}: {}".format(i, np.corrcoef(x, y_per)[1][0]))
+        y_per = np.random.permutation(data[:, 1]).reshape((-1, 1))
+        data_per = np.hstack((data[:, 0].reshape((-1,1)), y_per))
+        counter = counter + 1 if stat(data_per) > s else counter
     pval = counter / n
     if pval < alpha:
         print("pval is {} --> reject H_0".format(pval))
@@ -23,6 +26,21 @@ def permutation_test(x, y, n=1000, alpha=0.01):
         print("pval is {} --> accept H_0".format(pval))
 
 
+def hoeffding(data):
+    h_n = 0
+    for p in data:
+        o1 = data[data[:, 0] > p[0]]
+        o0 = data[data[:, 0] < p[0]]
+        o00 = o0[o0[:, 1] < p[1]]
+        o01 = o0[o0[:, 1] > p[1]]
+        o10 = o1[o1[:, 1] < p[1]]
+        o11 = o1[o1[:, 1] > p[1]]
+        h_n += ((len(o00) * len(o11)) - (len(o01) * len(o10)))**2
+    return float(h_n) / len(data)**4
+
 if __name__ == '__main__':
-    x, y = read_data()
-    permutation_test(x,y)
+    data = read_data()
+    # permutation_test(data, pearson)
+    permutation_test(data, hoeffding)
+    # h = hoeffding(data)
+    # print(h)
