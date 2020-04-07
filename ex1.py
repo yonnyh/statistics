f9ac4ba1c3b5d143edaad1b217650adc6a33e5f2
shmulik.or <gh778apk>
2020-04-07 14:28:10 +0300

    msg

--- ex1.py
+++ ex1.py
@@ -1,16 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def read_data():
     with open("data_EX1.txt") as file:
         data = np.loadtxt(file)
         return data
 
+
+def independent_data(size=1000):
+    np.random.seed(0)
+    x = np.random.randn(size).reshape((-1, 1))
+    np.random.seed(1)
+    y = np.random.randn(size).reshape((-1, 1))
+    return np.hstack((x, y))
+
+
+def circle_data(size=1000, rad=3):
+    theta = np.linspace(0, 2*np.pi, size)
+    x = (np.cos(theta) * rad).reshape((-1, 1))
+    y = (np.sin(theta) * rad).reshape((-1, 1))
+    return np.hstack((x, y))
+
+
 def pearson(data):
     return np.corrcoef(data[:, 0], data[:, 1])[1][0]
 
 
-def permutation_test(data, stat, n=10, alpha=0.01):
+def permutation_test(data, stat, n=1000, alpha=0.01):
     s = stat(data)
     print("basic test is {}".format(s))
 
@@ -19,6 +36,7 @@ def permutation_test(data, stat, n=10, alpha=0.01):
         y_per = np.random.permutation(data[:, 1]).reshape((-1, 1))
         data_per = np.hstack((data[:, 0].reshape((-1,1)), y_per))
         counter = counter + 1 if stat(data_per) > s else counter
+        # print(f"{i}: {stat(data_per)}")
     pval = counter / n
     if pval < alpha:
         print("pval is {} --> reject H_0".format(pval))
@@ -36,11 +54,9 @@ def hoeffding(data):
         o10 = o1[o1[:, 1] < p[1]]
         o11 = o1[o1[:, 1] > p[1]]
         h_n += ((len(o00) * len(o11)) - (len(o01) * len(o10)))**2
-    return float(h_n) / len(data)**4
+    return h_n / len(data)**4
 
 if __name__ == '__main__':
     data = read_data()
     # permutation_test(data, pearson)
     permutation_test(data, hoeffding)
-    # h = hoeffding(data)
-    # print(h)
