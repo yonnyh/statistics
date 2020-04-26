6f16f5c24df2a6e1c1432e1f0c82c5a79e390589
shmulik.or <gh778apk>
2020-04-26 12:12:54 +0300

    msg

--- ex1.py
+++ ex1.py
@@ -3,24 +3,7 @@ import matplotlib.pyplot as plt
 
 
 def read_data():
-    with open("data_EX1.txt") as file:
-        data = np.loadtxt(file)
-        return data
-
-
-def independent_data(size=1000):
-    np.random.seed(0)
-    x = np.random.randn(size).reshape((-1, 1))
-    np.random.seed(1)
-    y = np.random.randn(size).reshape((-1, 1))
-    return np.hstack((x, y))
-
-
-def circle_data(size=1000, rad=3):
-    theta = np.linspace(0, 2*np.pi, size)
-    x = (np.cos(theta) * rad).reshape((-1, 1))
-    y = (np.sin(theta) * rad).reshape((-1, 1))
-    return np.hstack((x, y))
+    return np.genfromtxt("data_EX1.txt", delimiter='\t')
 
 
 def pearson(data):
@@ -28,96 +11,107 @@ def pearson(data):
 
 
 def hoeffding(data):
-    h_n = 0
-    for p in data:
-        o1 = data[data[:, 0] > p[0]]
-        o0 = data[data[:, 0] < p[0]]
-        o00 = o0[o0[:, 1] < p[1]]
-        o01 = o0[o0[:, 1] > p[1]]
-        o10 = o1[o1[:, 1] < p[1]]
-        o11 = o1[o1[:, 1] > p[1]]
-        h_n += ((len(o00) * len(o11)) - (len(o01) * len(o10)))**2
-    return h_n / len(data)**4
+    o00, o01, o10, o11 = np.zeros(len(data)), np.zeros(len(data)), np.zeros(len(data)), np.zeros(len(data))
+    for i, p in enumerate(data):
+        o00[i] = np.sum(np.logical_and(data[:, 0] < p[0], data[:, 1] < p[1]))
+        o01[i] = np.sum(np.logical_and(data[:, 0] < p[0], data[:, 1] > p[1]))
+        o10[i] = np.sum(np.logical_and(data[:, 0] > p[0], data[:, 1] < p[1]))
+        o11[i] = np.sum(np.logical_and(data[:, 0] > p[0], data[:, 1] > p[1]))
+    return np.sum(np.power(o00 * o11 - o01 * o10, 2)) / len(data)**4
 
 
 def likelihood_ratio(data):
-    return np.prod(np.exp(((9 / 32) * (data[:, 0]**2 + data[:, 1]**2)) - ((15/16) * data[:, 0] * data[:, 1] * (np.e + np.e ** -1))))
+    return np.power(0.625, len(data)) * np.prod(np.exp((-9 / 32) * (np.power(data[:, 0], 2) + np.power(data[:, 1], 2))) *
+                    (np.exp((-15/16) * data[:, 0] * data[:, 1]) + np.exp((15/16) * data[:, 0] * data[:, 1])))
 
 
-def basic_stat(stat, data):
-    s = stat(data)
-    print("basic test is {}".format(s))
-    return s
+def log_likelihood_ratio(data):
+    return len(data) * np.log(0.625) + np.sum(np.log(np.exp((-9 / 32) * (np.power(data[:, 0], 2) + np.power(data[:, 1], 2))) *
+            (np.exp((-15 / 16) * data[:, 0] * data[:, 1]) + np.exp((15 / 16) * data[:, 0] * data[:, 1]))))
 
 
-def permutation_test(data, stat, n=100, alpha=0.01):
-    s = basic_stat(stat, data)
-
-    counter = 0
-    for i in range(0, n):
+def permutation_test(data, stat, alpha, M=1000, as_boolean=False):
+    s_true = stat(data)
+    res = np.zeros(M)
+    for i in range(M):
         y_per = np.random.permutation(data[:, 1]).reshape((-1, 1))
         data_per = np.hstack((data[:, 0].reshape((-1, 1)), y_per))
-        counter = counter + 1 if stat(data_per) > s else counter
-        # print(f"{i}: {stat(data_per)}")
-    pval = counter / n
-    return pval
-    # if pval < alpha:
-    #     print(f"pval is {pval} --> reject H_0")
-    # else:
-    #     print(f"pval is {pval} --> accept H_0")
+        res[i] = stat(data_per)
+    pval = np.count_nonzero(res > s_true) / M
+    return pval < alpha if as_boolean else pval
 
 
-def bootstrap(data, stat, n=1000):
-    # s = basic_stat(stat, data)
-    stats = []
-    for i in range(n):
+def bootstrap(data, stat, alpha, M=1000, as_boolean=False):
+    s_true = stat(data)
+    res = np.zeros(M)
+    for i in range(M):
         x_per = np.random.permutation(data[:, 0]).reshape((-1, 1))
         y_per = np.random.permutation(data[:, 1]).reshape((-1, 1))
         data_per = np.hstack((x_per, y_per))
-        stats.append(stat(data_per))
-    # print(f"the statistic value after bootstrap is {np.mean(stats)}, i.e the difference is {np.abs(s-np.mean(stats))}")
-    return np.mean(stats)
+        res[i] = stat(data_per)
+    pval = np.count_nonzero(res > s_true) / M
+    return pval < alpha if as_boolean else pval
 
 
-
-def q3b(independent=True):
-    mean = [0,0]
-    cov = [[1, 0.6], [0.6, 1]]
+def sample_data(h_0=True, R=100, M=1000, alpha=0.05):
     data_size = [n for n in range(100, 1001, 100)]
-    hoef, hoef_boot, lr, lr_boot = [], [], [], []
+    h, h_b, l, l_b = np.zeros(len(data_size)), np.zeros(len(data_size)), \
+                     np.zeros(len(data_size)), np.zeros(len(data_size))
 
-    for n in [1000]:
+    for n_i, n in enumerate(data_size):
         print(n)
-        if independent:
-            data = np.random.multivariate_normal(mean, [[1.6, 0], [0, 0.4]], n)
-        else:
-            # data = np.random.multivariate_normal(mean, cov, n)
-            data = read_data()
-        y = 2 * np.random.randint(2, size=n) - 1
-        data[:, 1] = y * data[:, 1]
-        basic_stat(hoeffding, data)
-        basic_stat(likelihood_ratio, data)
-        lr_boot.append(bootstrap(data, likelihood_ratio))
-        hoef.append(permutation_test(data, hoeffding))
-        hoef_boot.append(bootstrap(data, hoeffding))
-        lr.append(permutation_test(data, likelihood_ratio))
-    # plt.plot(data_size, hoef, "o-r", label="hoeffding")
-    # plt.plot(data_size, hoef_boot, "o--g", label="hoeffding bootstrap")
-    # plt.plot(data_size, hoef, "o-b", label="likelihood ratio")
-    # plt.plot(data_size, hoef, "o--y", label="likelihood ratio bootstrap")
-    print(hoef)
-    print(hoef_boot)
-    print(lr)
-    print(lr_boot)
-    # plt.grid(b=True, which='major', color='#666666', linestyle='-')
-    # plt.legend()
-    # plt.show()
+        for r in range(R):
+            if r % 10 == 0: print(f"r: {r}")
+            if h_0:
+                data = np.random.multivariate_normal(np.zeros(2), np.identity(2), n)
+            else:
+                data = np.random.multivariate_normal(np.zeros(2), [[1, 0.6], [0.6, 1]], n)
+                y = 2 * np.random.randint(2, size=n) - 1
+                data[:, 1] = y * data[:, 1]
+
+            if permutation_test(data, stat=hoeffding, alpha=alpha, M=M, as_boolean=True):
+                h[n_i] += 1
+            if bootstrap(data, stat=hoeffding, alpha=alpha, M=M, as_boolean=True):
+                h_b[n_i] += 1
+            if permutation_test(data, stat=log_likelihood_ratio, alpha=alpha, M=M, as_boolean=True):
+                l[n_i] += 1
+            if bootstrap(data, stat=log_likelihood_ratio, alpha=alpha, M=M, as_boolean=True):
+                l_b[n_i] += 1
+
+    h /= R
+    h_b /= R
+    l /= R
+    l_b /= R
+
+    print(h, h_b, l, l_b)
+
+    plt.plot(data_size, h, "o--r", label="hoeffding")
+    plt.plot(data_size, h_b, "o--g", label="hoeffding bootstrap")
+    plt.plot(data_size, l, "o--b", label="likelihood ratio")
+    plt.plot(data_size, l_b, "o--y", label="likelihood ratio bootstrap")
+    plt.title("type-1 error under the null H_0" if h_0 else "power under the alternative H_1")
+    plt.legend()
+    plt.show()
 
 
 if __name__ == '__main__':
     data = read_data()
-    # permutation_test(data, pearson)
-    # permutation_test(data, hoeffding)
-    # bootstrap(data, pearson)
-    # bootstrap(data, hoeffding)
-    q3b()
+    alpha_q2 = 0.01
+    alpha_q3 = 0.05
+
+    # q2a
+    print(f"2a: pearson coef is {pearson(data)}")
+    print(f"2a: p-value of permutation test with pearson statistic is {permutation_test(data, pearson, alpha_q2)}")
+
+    # q2b
+    print(f"2b: p-value of permutation test with hoeffding statistic is {permutation_test(data, hoeffding, alpha_q2)}")
+
+    # q2c
+    print(f"2c: p-value of bootstrap with pearson statistic is {bootstrap(data, pearson, alpha_q2)}")
+    print(f"2c: p-value of bootstrap with hoeffding statistic is {bootstrap(data, hoeffding, alpha_q2)}")
+
+    # q3b
+    sample_data(h_0=True, alpha=alpha_q3, R=100, M=1000)
+
+    # q3c
+    sample_data(h_0=False, alpha=alpha_q3, R=100, M=1000)
