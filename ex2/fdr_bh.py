92ef336f6f67a6b00ee15916affb4e7af11652c8
shmulik.or <gh778apk>
2020-05-06 13:02:16 +0300

    q5

--- ex2/fdr_bh.py
+++ ex2/fdr_bh.py
@@ -2,17 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
+m=1000
+m_0=500
+mu=2
 
-def generate_data(m=1000, m_0=500, mu=2, ro=0):
+def generate_data(rho=0):
 	mean = np.concatenate((np.zeros(m_0), np.ones(m - m_0) * mu))
-	cov = np.ones((m, m)) * ro
+	cov = np.ones((m, m)) * rho
 	np.fill_diagonal(cov, 1)
+	# np.random.seed(0)
 	return np.random.multivariate_normal(mean, cov)
 
 
 def histogram_and_densities(x, pi_0=0.5):
 	plt.hist(x, bins=100, density=True)
-	x_axis = np.linspace(-5, 5, 1000)
+	x_axis = np.linspace(-4, 4, 1000)
 	f0 = norm.pdf(x_axis, 0, 1)
 	f1 = norm.pdf(x_axis, 2, 1)
 	plt.plot(x_axis, pi_0 * f0)
@@ -20,6 +24,17 @@ def histogram_and_densities(x, pi_0=0.5):
 	plt.plot(x_axis, pi_0 * f0 + (1-pi_0) * f1)
 	plt.show()
 
+
+def fdr(c=0):
+	iters = 10
+	fdrs = np.zeros(iters)
+	for i in range(iters):
+		x = generate_data()
+		r_idx = np.where(np.logical_and(np.logical_and(x > -4, x < 4), x<=c))[0]
+		r = len(r_idx)
+		v = len(r_idx[r_idx < m_0])
+		fdrs[i] = v / r
+	print(np.mean(fdrs))
 
 
 
@@ -44,5 +59,8 @@ if __name__ == '__main__':
 	# alpha = 0.05
 	# idxs = fdr_bh(pval_list, alpha)
 	# print(pval_list[idxs])
+
+	# q5
 	x = generate_data()
 	histogram_and_densities(x)
+	fdr(x)
